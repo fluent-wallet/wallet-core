770ec4882e0a4356c6db4ee9b89d967cb8834d37
iosh <ioshx@foxmail.com>
2024-10-28 17:43:28 +0800

    chore: clean backgroundMethodWhenPopup fn

--- core/wallet/src/utils/browser.ts
+++ core/wallet/src/utils/browser.ts
@@ -1,8 +1,7 @@
-import type WalletClass from "../index";
-import browser from "webextension-polyfill";
-import type { EXTENSION_TYPE } from "../index";
+import browser from 'webextension-polyfill';
+import type { EXTENSION_TYPE } from '../index';
 
-export type UnknownTarget = Record<string, PropertyDescriptor["value"]>;
+export type UnknownTarget = Record<string, PropertyDescriptor['value']>;
 
 /**
  * Proxy wallet methods, if it's popup, we send message to background to execute the method.
@@ -10,27 +9,26 @@ export type UnknownTarget = Record<string, PropertyDescriptor["value"]>;
  * @param extensionType
  * @returns
  */
-export function backgroundMethodWhenPopup<
-	T extends Record<string | symbol, any>,
->(fns: T, extensionType: EXTENSION_TYPE): T {
-	return new Proxy(fns, {
-		get: (target, prop) => {
-			return async (...args: any[]) => {
-				/**
-				 * if this is popup, we send message to background to execute the method.
-				 */
-				if (extensionType === "popup") {
-					const response = await browser.runtime.sendMessage({
-						method: prop,
-						args,
-					});
-					return response;
-				}
-				/**
-				 * if this is not popup, just call the original method
-				 */
-				return target[prop](...args);
-			};
-		},
-	});
+export function backgroundMethodWhenPopup<T extends Record<string | symbol, any>>(fns: T, extensionType: EXTENSION_TYPE): T {
+  return new Proxy(fns, {
+    get: (target, prop) => {
+      return async (...args: any[]) => {
+        /**
+         * if this is not popup, just call the original method
+         */
+        if (extensionType === 'background') {
+          return target[prop](...args);
+        }
+
+        /**
+         * if this is popup, we send message to background to execute the method.
+         */
+        const response = await browser.runtime.sendMessage({
+          method: prop,
+          args,
+        });
+        return response;
+      };
+    },
+  });
 }
